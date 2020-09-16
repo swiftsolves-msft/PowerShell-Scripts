9faf34f5883d12cfd3ad475b0049a1eed9709459
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2020-09-16 16:34:48 -0400

    Add files via upload

---
+++ Export-AzLaSavedSearches.ps1
@@ -0,0 +1,31 @@
+﻿Login-AzAccount
+
+
+$rgname = "YOUR RG NAME"
+$workspacename = "YOUR WORKSPACE NAME"
+
+
+$savedsearches = Get-AzOperationalInsightsSavedSearch -ResourceGroupName $rgname -WorkspaceName $workspacename
+
+$LogFilePath = "c:\temp\kql\"
+
+foreach ($savedsearch in $savedsearches.Value) {
+
+
+    $savedkql = $savedsearch.Properties.Query
+    $filename = $savedsearch.Properties.Category + '_-_' + $savedsearch.Properties.DisplayName
+
+    $LogFile = $LogFilePath + $filename + ".txt"
+    
+    try {
+        $run = Write-Output $savedkql | Out-File -FilePath $LogFile -ErrorAction Stop
+    }
+    catch {
+        $newguid = New-Guid
+        $filename = $savedsearch.Properties.Category + '_-_' + $newguid
+    
+        $LogFile = $LogFilePath + $filename + ".txt"
+        $run = Write-Output $savedkql | Out-File -FilePath $LogFile
+    }
+
+}
