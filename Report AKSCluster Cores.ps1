e6cedeb8b3e4deed99ce1cd8eea4029d0b4cff56
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2022-01-04 21:43:59 -0500

    Add files via upload

---
+++ Report AKSCluster Cores.ps1
@@ -0,0 +1,597 @@
+﻿Get-AzVMSize -Location "Central US"
+
+$subs = Get-AzSubscription
+
+#$csvrawreq = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/swiftsolves-msft/kql/main/lookup/vmsizeinfo.csv" -OutFile vmsizeinfo.csv
+
+#$vmsizeinfo = Import-Csv vmsizeinfo.csv
+
+$outputFile = "akscorereport.csv"
+
+#$string = "$($sub.Name),$($akscluster.name),$($VM.Location),$($akscluster.ResourceGroupName),$($aksvmsize),$($akscores)"
+
+$stringstart = "Subscription,AKSName,Location,ResourceGroupName,AKSSize,NumberOfCores"
+$stringstart | Out-File $outputFile -append -force
+
+foreach ($sub in $subs){
+
+    Set-AzContext -Subscription $sub.id
+
+    $aksclusters = Get-AzAksCluster
+
+    foreach ($akscluster in $aksclusters) {
+    
+        $aksvmsize = $akscluster.AgentPoolProfiles.VmSize
+
+        $akscores = switch ($aksvmsize){
+        
+            Standard_B1ls {1}
+            Standard_B1ms {1}
+            Standard_B1s {1}
+            Standard_B2ms {2}
+            Standard_B2s {2}
+            Standard_B4ms {4}
+            Standard_B8ms {8}
+            Standard_B12ms {12}
+            Standard_B16ms {16}
+            Standard_B20ms {20}
+            Standard_DS1_v2 {1}
+            Standard_DS2_v2 {2}
+            Standard_DS3_v2 {4}
+            Standard_DS4_v2 {8}
+            Standard_DS5_v2 {16}
+            Standard_DS11-1_v2 {2}
+            Standard_DS11_v2 {2}
+            Standard_DS12-1_v2 {4}
+            Standard_DS12-2_v2 {4}
+            Standard_DS12_v2 {4}
+            Standard_DS13-2_v2 {8}
+            Standard_DS13-4_v2 {8}
+            Standard_DS13_v2 {8}
+            Standard_DS14-4_v2 {16}
+            Standard_DS14-8_v2 {16}
+            Standard_DS14_v2 {16}
+            Standard_DS15_v2 {20}
+            Standard_DS2_v2_Promo {2}
+            Standard_DS3_v2_Promo {4}
+            Standard_DS4_v2_Promo {8}
+            Standard_DS5_v2_Promo {16}
+Standard_DS11_v2_Promo {2}
+Standard_DS12_v2_Promo {4}
+Standard_DS13_v2_Promo {8}
+Standard_DS14_v2_Promo {16}
+Standard_F1s {1}
+Standard_F2s {2}
+Standard_F4s {4}
+Standard_F8s {8}
+Standard_F16s {16}
+Standard_D2s_v3 {2}
+Standard_D4s_v3 {4}
+Standard_D8s_v3 {8}
+Standard_D16s_v3 {16}
+Standard_D32s_v3 {32}
+Standard_D2a_v4 {2}
+Standard_D4a_v4 {4}
+Standard_D8a_v4 {8}
+Standard_D16a_v4 {16}
+Standard_D32a_v4 {32}
+Standard_D48a_v4 {48}
+Standard_D64a_v4 {64}
+Standard_D96a_v4 {96}
+Standard_D2as_v4 {2}
+Standard_D4as_v4 {4}
+Standard_D8as_v4 {8}
+Standard_D16as_v4 {16}
+Standard_D32as_v4 {32}
+Standard_D48as_v4 {48}
+Standard_D64as_v4 {64}
+Standard_D96as_v4 {96}
+Standard_E2a_v4 {2}
+Standard_E4a_v4 {4}
+Standard_E8a_v4 {8}
+Standard_E16a_v4 {16}
+Standard_E20a_v4 {20}
+Standard_E32a_v4 {32}
+Standard_E48a_v4 {48}
+Standard_E64a_v4 {64}
+Standard_E96a_v4 {96}
+Standard_E2as_v4 {2}
+Standard_E4-2as_v4 {4}
+Standard_E4as_v4 {4}
+Standard_E8-2as_v4 {8}
+Standard_E8-4as_v4 {8}
+Standard_E8as_v4 {8}
+Standard_E16-4as_v4 {16}
+Standard_E16-8as_v4 {16}
+Standard_E16as_v4 {16}
+Standard_E20as_v4 {20}
+Standard_E32-8as_v4 {32}
+Standard_E32-16as_v4 {32}
+Standard_E32as_v4 {32}
+Standard_E48as_v4 {48}
+Standard_E64-16as_v4 {64}
+Standard_E64-32as_v4 {64}
+Standard_E64as_v4 {64}
+Standard_E96-24as_v4 {96}
+Standard_E96-48as_v4 {96}
+Standard_E96as_v4 {96}
+Standard_E96ias_v4 {96}
+Standard_D1_v2 {1}
+Standard_D2_v2 {2}
+Standard_D3_v2 {4}
+Standard_D4_v2 {8}
+Standard_D5_v2 {16}
+Standard_D11_v2 {2}
+Standard_D12_v2 {4}
+Standard_D13_v2 {8}
+Standard_D14_v2 {16}
+Standard_D15_v2 {20}
+Standard_D2_v2_Promo {2}
+Standard_D3_v2_Promo {4}
+Standard_D4_v2_Promo {8}
+Standard_D5_v2_Promo {16}
+Standard_D11_v2_Promo {2}
+Standard_D12_v2_Promo {4}
+Standard_D13_v2_Promo {8}
+Standard_D14_v2_Promo {16}
+Standard_F1 {1}
+Standard_F2 {2}
+Standard_F4 {4}
+Standard_F8 {8}
+Standard_F16 {16}
+Standard_A1_v2 {1}
+Standard_A2m_v2 {2}
+Standard_A2_v2 {2}
+Standard_A4m_v2 {4}
+Standard_A4_v2 {4}
+Standard_A8m_v2 {8}
+Standard_A8_v2 {8}
+Standard_D2_v3 {2}
+Standard_D4_v3 {4}
+Standard_D8_v3 {8}
+Standard_D16_v3 {16}
+Standard_D32_v3 {32}
+Standard_D48_v3 {48}
+Standard_D64_v3 {64}
+Standard_D48s_v3 {48}
+Standard_D64s_v3 {64}
+Standard_E2_v3 {2}
+Standard_E4_v3 {4}
+Standard_E8_v3 {8}
+Standard_E16_v3 {16}
+Standard_E20_v3 {20}
+Standard_E32_v3 {32}
+Standard_E48_v3 {48}
+Standard_E64_v3 {64}
+Standard_E2s_v3 {2}
+Standard_E4-2s_v3 {4}
+Standard_E4s_v3 {4}
+Standard_E8-2s_v3 {8}
+Standard_E8-4s_v3 {8}
+Standard_E8s_v3 {8}
+Standard_E16-4s_v3 {16}
+Standard_E16-8s_v3 {16}
+Standard_E16s_v3 {16}
+Standard_E20s_v3 {20}
+Standard_E32-8s_v3 {32}
+Standard_E32-16s_v3 {32}
+Standard_E32s_v3 {32}
+Standard_E48s_v3 {48}
+Standard_E64-16s_v3 {64}
+Standard_E64-32s_v3 {64}
+Standard_E64s_v3 {64}
+Standard_A0 {1}
+Standard_A1 {1}
+Standard_A2 {2}
+Standard_A3 {4}
+Standard_A5 {2}
+Standard_A4 {8}
+Standard_A6 {4}
+Standard_A7 {8}
+Basic_A0 {1}
+Basic_A1 {1}
+Basic_A2 {2}
+Basic_A3 {4}
+Basic_A4 {8}
+Standard_E64i_v3 {64}
+Standard_E64is_v3 {64}
+Standard_M208ms_v2 {208}
+Standard_M208s_v2 {208}
+Standard_M416-208s_v2 {416}
+Standard_M416s_v2 {416}
+Standard_M416-208ms_v2 {416}
+Standard_M416ms_v2 {416}
+Standard_H8 {8}
+Standard_H8_Promo {8}
+Standard_H16 {16}
+Standard_H16_Promo {16}
+Standard_H8m {8}
+Standard_H8m_Promo {8}
+Standard_H16m {16}
+Standard_H16m_Promo {16}
+Standard_H16r {16}
+Standard_H16r_Promo {16}
+Standard_H16mr {16}
+Standard_H16mr_Promo {16}
+Standard_D1 {1}
+Standard_D2 {2}
+Standard_D3 {4}
+Standard_D4 {8}
+Standard_D11 {2}
+Standard_D12 {4}
+Standard_D13 {8}
+Standard_D14 {16}
+Standard_NV6 {6}
+Standard_NV12 {12}
+Standard_NV24 {24}
+Standard_NV6_Promo {6}
+Standard_NV12_Promo {12}
+Standard_NV24_Promo {24}
+Standard_NC4as_T4_v3 {4}
+Standard_NC8as_T4_v3 {8}
+Standard_NC16as_T4_v3 {16}
+Standard_NC64as_T4_v3 {64}
+Standard_NV6s_v2 {6}
+Standard_NV12s_v2 {12}
+Standard_NV24s_v2 {24}
+Standard_NV12s_v3 {12}
+Standard_NV24s_v3 {24}
+Standard_NV48s_v3 {48}
+Standard_HB120rs_v2 {120}
+Standard_D2ds_v4 {2}
+Standard_D4ds_v4 {4}
+Standard_D8ds_v4 {8}
+Standard_D16ds_v4 {16}
+Standard_D32ds_v4 {32}
+Standard_D48ds_v4 {48}
+Standard_D64ds_v4 {64}
+Standard_D2ds_v5 {2}
+Standard_D4ds_v5 {4}
+Standard_D8ds_v5 {8}
+Standard_D16ds_v5 {16}
+Standard_D32ds_v5 {32}
+Standard_D48ds_v5 {48}
+Standard_D64ds_v5 {64}
+Standard_D96ds_v5 {96}
+Standard_D2d_v4 {2}
+Standard_D4d_v4 {4}
+Standard_D8d_v4 {8}
+Standard_D16d_v4 {16}
+Standard_D32d_v4 {32}
+Standard_D48d_v4 {48}
+Standard_D64d_v4 {64}
+Standard_D2d_v5 {2}
+Standard_D4d_v5 {4}
+Standard_D8d_v5 {8}
+Standard_D16d_v5 {16}
+Standard_D32d_v5 {32}
+Standard_D48d_v5 {48}
+Standard_D64d_v5 {64}
+Standard_D96d_v5 {96}
+Standard_D2s_v4 {2}
+Standard_D4s_v4 {4}
+Standard_D8s_v4 {8}
+Standard_D16s_v4 {16}
+Standard_D32s_v4 {32}
+Standard_D48s_v4 {48}
+Standard_D64s_v4 {64}
+Standard_D2s_v5 {2}
+Standard_D4s_v5 {4}
+Standard_D8s_v5 {8}
+Standard_D16s_v5 {16}
+Standard_D32s_v5 {32}
+Standard_D48s_v5 {48}
+Standard_D64s_v5 {64}
+Standard_D96s_v5 {96}
+Standard_D2_v4 {2}
+Standard_D4_v4 {4}
+Standard_D8_v4 {8}
+Standard_D16_v4 {16}
+Standard_D32_v4 {32}
+Standard_D48_v4 {48}
+Standard_D64_v4 {64}
+Standard_D2_v5 {2}
+Standard_D4_v5 {4}
+Standard_D8_v5 {8}
+Standard_D16_v5 {16}
+Standard_D32_v5 {32}
+Standard_D48_v5 {48}
+Standard_D64_v5 {64}
+Standard_D96_v5 {96}
+Standard_E2ds_v4 {2}
+Standard_E4-2ds_v4 {4}
+Standard_E4ds_v4 {4}
+Standard_E8-2ds_v4 {8}
+Standard_E8-4ds_v4 {8}
+Standard_E8ds_v4 {8}
+Standard_E16-4ds_v4 {16}
+Standard_E16-8ds_v4 {16}
+Standard_E16ds_v4 {16}
+Standard_E20ds_v4 {20}
+Standard_E32-8ds_v4 {32}
+Standard_E32-16ds_v4 {32}
+Standard_E32ds_v4 {32}
+Standard_E48ds_v4 {48}
+Standard_E64-16ds_v4 {64}
+Standard_E64-32ds_v4 {64}
+Standard_E64ds_v4 {64}
+Standard_E80ids_v4 {80}
+Standard_E2ds_v5 {2}
+Standard_E4-2ds_v5 {4}
+Standard_E4ds_v5 {4}
+Standard_E8-2ds_v5 {8}
+Standard_E8-4ds_v5 {8}
+Standard_E8ds_v5 {8}
+Standard_E16-4ds_v5 {16}
+Standard_E16-8ds_v5 {16}
+Standard_E16ds_v5 {16}
+Standard_E20ds_v5 {20}
+Standard_E32-8ds_v5 {32}
+Standard_E32-16ds_v5 {32}
+Standard_E32ds_v5 {32}
+Standard_E48ds_v5 {48}
+Standard_E64-16ds_v5 {64}
+Standard_E64-32ds_v5 {64}
+Standard_E64ds_v5 {64}
+Standard_E96-24ds_v5 {96}
+Standard_E96-48ds_v5 {96}
+Standard_E96ds_v5 {96}
+Standard_E104ids_v5 {104}
+Standard_E2d_v4 {2}
+Standard_E4d_v4 {4}
+Standard_E8d_v4 {8}
+Standard_E16d_v4 {16}
+Standard_E20d_v4 {20}
+Standard_E32d_v4 {32}
+Standard_E48d_v4 {48}
+Standard_E64d_v4 {64}
+Standard_E2d_v5 {2}
+Standard_E4d_v5 {4}
+Standard_E8d_v5 {8}
+Standard_E16d_v5 {16}
+Standard_E20d_v5 {20}
+Standard_E32d_v5 {32}
+Standard_E48d_v5 {48}
+Standard_E64d_v5 {64}
+Standard_E96d_v5 {96}
+Standard_E104id_v5 {104}
+Standard_E2s_v4 {2}
+Standard_E4-2s_v4 {4}
+Standard_E4s_v4 {4}
+Standard_E8-2s_v4 {8}
+Standard_E8-4s_v4 {8}
+Standard_E8s_v4 {8}
+Standard_E16-4s_v4 {16}
+Standard_E16-8s_v4 {16}
+Standard_E16s_v4 {16}
+Standard_E20s_v4 {20}
+Standard_E32-8s_v4 {32}
+Standard_E32-16s_v4 {32}
+Standard_E32s_v4 {32}
+Standard_E48s_v4 {48}
+Standard_E64-16s_v4 {64}
+Standard_E64-32s_v4 {64}
+Standard_E64s_v4 {64}
+Standard_E80is_v4 {80}
+Standard_E2s_v5 {2}
+Standard_E4-2s_v5 {4}
+Standard_E4s_v5 {4}
+Standard_E8-2s_v5 {8}
+Standard_E8-4s_v5 {8}
+Standard_E8s_v5 {8}
+Standard_E16-4s_v5 {16}
+Standard_E16-8s_v5 {16}
+Standard_E16s_v5 {16}
+Standard_E20s_v5 {20}
+Standard_E32-8s_v5 {32}
+Standard_E32-16s_v5 {32}
+Standard_E32s_v5 {32}
+Standard_E48s_v5 {48}
+Standard_E64-16s_v5 {64}
+Standard_E64-32s_v5 {64}
+Standard_E64s_v5 {64}
+Standard_E96-24s_v5 {96}
+Standard_E96-48s_v5 {96}
+Standard_E96s_v5 {96}
+Standard_E104is_v5 {104}
+Standard_E2_v4 {2}
+Standard_E4_v4 {4}
+Standard_E8_v4 {8}
+Standard_E16_v4 {16}
+Standard_E20_v4 {20}
+Standard_E32_v4 {32}
+Standard_E48_v4 {48}
+Standard_E64_v4 {64}
+Standard_E2_v5 {2}
+Standard_E4_v5 {4}
+Standard_E8_v5 {8}
+Standard_E16_v5 {16}
+Standard_E20_v5 {20}
+Standard_E32_v5 {32}
+Standard_E48_v5 {48}
+Standard_E64_v5 {64}
+Standard_E96_v5 {96}
+Standard_E104i_v5 {104}
+Standard_F2s_v2 {2}
+Standard_F4s_v2 {4}
+Standard_F8s_v2 {8}
+Standard_F16s_v2 {16}
+Standard_F32s_v2 {32}
+Standard_F48s_v2 {48}
+Standard_F64s_v2 {64}
+Standard_F72s_v2 {72}
+Standard_NC6s_v2 {6}
+Standard_NC12s_v2 {12}
+Standard_NC24rs_v2 {24}
+Standard_NC24s_v2 {24}
+Standard_NC6 {6}
+Standard_NC12 {12}
+Standard_NC24 {24}
+Standard_NC24r {24}
+Standard_NC6_Promo {6}
+Standard_NC12_Promo {12}
+Standard_NC24_Promo {24}
+Standard_NC24r_Promo {24}
+Standard_DS1 {1}
+Standard_DS2 {2}
+Standard_DS3 {4}
+Standard_DS4 {8}
+Standard_DS11 {2}
+Standard_DS12 {4}
+Standard_DS13 {8}
+Standard_DS14 {16}
+Standard_L8s_v2 {8}
+Standard_L16s_v2 {16}
+Standard_L32s_v2 {32}
+Standard_L48s_v2 {48}
+Standard_L64s_v2 {64}
+Standard_L80s_v2 {80}
+Standard_M64 {64}
+Standard_M64m {64}
+Standard_M128 {128}
+Standard_M128m {128}
+Standard_M8-2ms {8}
+Standard_M8-4ms {8}
+Standard_M8ms {8}
+Standard_M16-4ms {16}
+Standard_M16-8ms {16}
+Standard_M16ms {16}
+Standard_M32-8ms {32}
+Standard_M32-16ms {32}
+Standard_M32ls {32}
+Standard_M32ms {32}
+Standard_M32ts {32}
+Standard_M64-16ms {64}
+Standard_M64-32ms {64}
+Standard_M64ls {64}
+Standard_M64ms {64}
+Standard_M64s {64}
+Standard_M128-32ms {128}
+Standard_M128-64ms {128}
+Standard_M128ms {128}
+Standard_M128s {128}
+Standard_M32ms_v2 {32}
+Standard_M64ms_v2 {64}
+Standard_M64s_v2 {64}
+Standard_M128ms_v2 {128}
+Standard_M128s_v2 {128}
+Standard_M192ims_v2 {192}
+Standard_M192is_v2 {192}
+Standard_M32dms_v2 {32}
+Standard_M64dms_v2 {64}
+Standard_M64ds_v2 {64}
+Standard_M128dms_v2 {128}
+Standard_M128ds_v2 {128}
+Standard_M192idms_v2 {192}
+Standard_M192ids_v2 {192}
+Standard_DC8_v2 {8}
+Standard_DC1s_v2 {1}
+Standard_DC2s_v2 {2}
+Standard_DC4s_v2 {4}
+Standard_ND40rs_v2 {40}
+Standard_FX4mds {4}
+Standard_FX12mds {12}
+Standard_FX24mds {24}
+Standard_FX36mds {36}
+Standard_FX48mds {48}
+Standard_NP10s {10}
+Standard_NP20s {20}
+Standard_NP40s {40}
+Standard_D2as_v5 {2}
+Standard_D4as_v5 {4}
+Standard_D8as_v5 {8}
+Standard_D16as_v5 {16}
+Standard_D32as_v5 {32}
+Standard_D48as_v5 {48}
+Standard_D64as_v5 {64}
+Standard_D96as_v5 {96}
+Standard_E2as_v5 {2}
+Standard_E4-2as_v5 {4}
+Standard_E4as_v5 {4}
+Standard_E8-2as_v5 {8}
+Standard_E8-4as_v5 {8}
+Standard_E8as_v5 {8}
+Standard_E16-4as_v5 {16}
+Standard_E16-8as_v5 {16}
+Standard_E16as_v5 {16}
+Standard_E32-8as_v5 {32}
+Standard_E32-16as_v5 {32}
+Standard_E32as_v5 {32}
+Standard_E48as_v5 {48}
+Standard_E64-16as_v5 {64}
+Standard_E64-32as_v5 {64}
+Standard_E64as_v5 {64}
+Standard_E96-24as_v5 {96}
+Standard_E96-48as_v5 {96}
+Standard_E96as_v5 {96}
+Standard_E112ias_v5 {112}
+Standard_D2ads_v5 {2}
+Standard_D4ads_v5 {4}
+Standard_D8ads_v5 {8}
+Standard_D16ads_v5 {16}
+Standard_D32ads_v5 {32}
+Standard_D48ads_v5 {48}
+Standard_D64ads_v5 {64}
+Standard_D96ads_v5 {96}
+Standard_E2ads_v5 {2}
+Standard_E4-2ads_v5 {4}
+Standard_E4ads_v5 {4}
+Standard_E8-2ads_v5 {8}
+Standard_E8-4ads_v5 {8}
+Standard_E8ads_v5 {8}
+Standard_E16-4ads_v5 {16}
+Standard_E16-8ads_v5 {16}
+Standard_E16ads_v5 {16}
+Standard_E20ads_v5 {20}
+Standard_E32-8ads_v5 {32}
+Standard_E32-16ads_v5 {32}
+Standard_E32ads_v5 {32}
+Standard_E48ads_v5 {48}
+Standard_E64-16ads_v5 {64}
+Standard_E64-32ads_v5 {64}
+Standard_E64ads_v5 {64}
+Standard_E96-24ads_v5 {96}
+Standard_E96-48ads_v5 {96}
+Standard_E96ads_v5 {96}
+Standard_E112iads_v5 {112}
+Standard_HC44-16rs {44}
+Standard_HC44-32rs {44}
+Standard_HC44rs {44}
+Standard_ND6s {6}
+Standard_ND12s {12}
+Standard_ND24rs {24}
+Standard_ND24s {24}
+Standard_DC2s {2}
+Standard_DC4s {4}
+Standard_HB120-16rs_v3 {120}
+Standard_HB120-32rs_v3 {120}
+Standard_HB120-64rs_v3 {120}
+Standard_HB120-96rs_v3 {120}
+Standard_HB120rs_v3 {120}
+Standard_NV4as_v4 {4}
+Standard_NV8as_v4 {8}
+Standard_NV16as_v4 {16}
+Standard_NV32as_v4 {32}
+Standard_NC6s_v3 {6}
+Standard_NC12s_v3 {12}
+Standard_NC24rs_v3 {24}
+Standard_NC24s_v3 {24}
+Standard_PB6s {6}
+Standard_HB60-15rs {60}
+Standard_HB60-30rs {60}
+Standard_HB60-45rs {60}
+            Standard_HB60rs {60}
+            Standard_ND96asr_v4 {96}
+            Standard_ND40s_v3 {40}
+            default {$null}
+
+        }
+ 
+    
+        $string = "$($sub.Name),$($akscluster.name),$($VM.Location),$($akscluster.ResourceGroupName),$($aksvmsize),$($akscores)"
+
+        $string | Out-File $outputFile -append -force
+
+    }
+
+}
