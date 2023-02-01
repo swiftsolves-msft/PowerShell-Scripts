cffabac64fa3669a4b7c322b68d13f4ba6ae89e6
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2023-02-01 15:46:47 -0500

    Add files via upload

---
+++ DefenderforPricing.ps1
@@ -0,0 +1,26 @@
+﻿
+$Free = 'Free'
+$Standard = 'Standard'
+
+$subs = Get-AzSubscription
+
+foreach($sub in $subs){
+
+    # set a particular subscription context to search for VMs
+    Set-AzContext -Subscription $Sub.Id
+
+    Get-AzSecurityPricing -Name CloudPosture
+    # Set-AzSecurityPricing -Name CloudPosture -PricingTier $Free
+
+    # Set-AzSecurityPricing -Name VirtualMachines -PricingTier $
+    # Set-AzSecurityPricing -Name SqlServers -PricingTier $
+    # Set-AzSecurityPricing -Name AppServices -PricingTier $
+    # Set-AzSecurityPricing -Name StorageAccounts -PricingTier $
+    # Set-AzSecurityPricing -Name SqlServerVirtualMachines -PricingTier $
+    # Set-AzSecurityPricing -Name KeyVaults -PricingTier $
+    # Set-AzSecurityPricing -Name Dns -PricingTier $
+    # Set-AzSecurityPricing -Name Arm -PricingTier $
+    # Set-AzSecurityPricing -Name OpenSourceRelationalDatabases -PricingTier $
+    # Set-AzSecurityPricing -Name CosmosDbs -PricingTier $
+    # Set-AzSecurityPricing -Name Containers -PricingTier $
+}
